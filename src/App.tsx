a469a2eb5b6d96929ca9aa512fb538ca9377e506
@mazh <mazh@sletat.ru>
2025-03-31 22:51:42 +0300

    add improvements and fixes

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
 import MainBlock from "./pages/main/main";
-import {Route, Routes} from "react-router-dom";
 import CompetenciesTemplate from "./pages/competenciesTemplate/competenciesTemplate";
-import {store} from "./store/store";
+import { store } from "./store/store";
 
 export default function App() {
-    return (
-        <div className="App">
-            <Routes>
-                <Route path="" element={<MainBlock />} />
-                {store.competencies.map((competence)=>(
-                    <Route path={`/competence/${competence.pathLink}`} element={<CompetenciesTemplate competence={competence}/>}/>
-                ))}
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="" element={<MainBlock />} />
+        {store.competencies.map((competence) => (
+          <Route
+            path={`/competence/${competence.pathLink}`}
+            element={<CompetenciesTemplate competence={competence} />}
+          />
+        ))}
+      </Routes>
+    </div>
+  );
 }
--- src/pages/main/AboutCompany/AboutCompany.tsx
+++ src/pages/main/AboutCompany/AboutCompany.tsx
@@ -1,23 +1,32 @@
-import style from './style.module.scss'
+import style from "./style.module.scss";
 
 export default function AboutCompany() {
-    return (
-        <div className={style.wrapper}>
-            <div className={style['company-information']}>
-                <h1>О компании</h1>
-                <div className={style.infomation}>
-                    <hr/>
-                    <p className={style.description}>
-                        Мы занимаемся <span>IT-разработкой</span> и развитием продуктов с акцентом на бизнес-рост наших партнеров. В
-                        нашей команде нет проектных менеджеров в классическом смысле — <span>все наши специалисты имеют
-                        продуктовый опыт и в первую очередь заботятся о стратегическом развитии вашего бизнеса.</span> Мы <span>ценим
-                    </span>
-                        долгосрочные отношения с партнерами и <span>стремимся расти вместе</span> с ними. Все, что мы делаем, мы
-                        делаем с таким же <span>вниманием</span> и <span>качеством</span>, как для себя.
-                    </p>
-                    <p className={style.date}>20—24</p>
-                </div>
-            </div>
+  return (
+    <div className={style.wrapper}>
+      <div className={style["company-information"]}>
+        <h1>О компании</h1>
+        <div className={style.infomation}>
+          <hr />
+          <p className={style.description}>
+            Мы занимаемся <span>IT-разработкой</span> и развитием продуктов с
+            акцентом на бизнес-рост наших партнеров. <br />
+            <br />В нашей команде нет проектных менеджеров в классическом смысле
+            —{" "}
+            <span>
+              все наши специалисты имеют продуктовый опыт и в первую очередь
+              заботятся о стратегическом развитии вашего бизнеса.
+            </span>{" "}
+            <br />
+            <br />
+            Мы <span>ценим </span>
+            долгосрочные отношения с партнерами и{" "}
+            <span>стремимся расти вместе</span> с ними. Все, что мы делаем, мы
+            делаем с таким же <span>вниманием</span> и <span>качеством</span>,
+            как для себя.
+          </p>
+          <p className={style.date}>20—24</p>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
